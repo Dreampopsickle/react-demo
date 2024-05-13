eadde9d4387114baabef3355c4549f5e776e0d34
Dreampopsickle <132526661+Dreampopsickle@users.noreply.github.com>
2024-05-13 13:37:15 -0400

    add feature

--- starting-project/src/components/PostList.jsx
+++ starting-project/src/components/PostList.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
 
-function PostList() {
+function PostList({ isPosting, onStopPosting }) {
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
 
@@ -17,10 +18,15 @@ function PostList() {
 
   return (
     <>
-      <NewPost
-        onBodyChange={bodyChangeHandler}
-        onAuthorChange={authorChangeHandler}
-      />
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
+
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="BBL Drizzy" body="I'm a bitch" />
